268ae1e7c5710d892534e7c2640259fc4876f080
Keven <keven.santos.sz@gmail.com>
2020-05-16 11:10:47 -0300

    Updates

---
+++ precache-manifest.80dfc75b9b57c676409ef5a1b7c2165e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06478c7a9e5925e9e7b3e7315e8a328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35263a93b764eb4b83d0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae5892438c41fcfda944",
+    "url": "/static/js/2.4f9cd052.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4f9cd052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35263a93b764eb4b83d0",
+    "url": "/static/js/main.b883713c.chunk.js"
+  },
+  {
+    "revision": "e1a9aa2aa18554368e6c",
+    "url": "/static/js/runtime-main.dffab8f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
